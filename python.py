9c78b46b9bc4d95a99f61761f8c7864ba00c38b6
UltimateBoris2 <167884942+UltimateBoris2@users.noreply.github.com>
2024-10-01 17:15:57 +0300

    jpiortjsogijrwapoe
    rgjoiersjogiujhtijyfuhoiuheriufh

---
+++ python.py
@@ -0,0 +1,5 @@
+def personal_info_gatherer(name, age):
+    frase = 'My name is' + ' ' + str(name) + '.' + 'My age is' + ' ' + str(age)
+    return frase
+
+print(personal_info_gatherer('someone', '99'))
